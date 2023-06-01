d4036101af45a07d809f1b8c23323a50491795f1
Shashwat Singh <shashwat2048@gmail.com>
2023-06-01 23:10:21 +0530

    "two sums"

--- src/easy/Q1_Two_sums.java
+++ src/easy/Q1_Two_sums.java
@@ -4,8 +4,8 @@ public class Q1_Two_sums {
 
     public static void main(String[] args) {
         int target = 9;
-        int nums[] = {1,2,3,4,5,6,7,8,9};
-        int [] result = twoSum(nums, target);
+        int[] nums = {1,2,3,4,5,6,7,8,9};
+        int[] result = twoSum(nums, target);
 
         if (result != null) {
             System.out.println("Indexes: " + result[0] + ", " + result[1]);

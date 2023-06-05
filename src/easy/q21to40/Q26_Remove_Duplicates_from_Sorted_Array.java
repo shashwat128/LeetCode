52b05c7c9a391f59632ece16af085407f59e073b
Shashwat Singh <shashwat2048@gmail.com>
2023-06-05 23:56:32 +0530

    "Ading questions and solving merge two lists with help"

---
+++ src/easy/q21to40/Q26_Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,97 @@
+package easy.q21to40;
+
+public class Q26_Remove_Duplicates_from_Sorted_Array {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 2, 3, 4, 5, 5};
+
+        int length = removeDuplicates(nums);
+
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] = Integer.MAX_VALUE;
+            }
+        }
+        int j = 0;
+        int k = 0;
+        while (j < nums.length) {
+            if (nums[j] == Integer.MAX_VALUE) {
+                k = j + 1;
+                while (k < nums.length && nums[k] == Integer.MAX_VALUE) {
+                    k++;
+                }
+                if (k < nums.length) {
+                    nums[j] = nums[k];
+                    nums[k] = Integer.MAX_VALUE;
+                }
+            }
+            j++;
+        }
+        int length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != Integer.MAX_VALUE) {
+                length++;
+            }
+        }
+
+        return length;
+    }
+}
+
+/*
+*
+* 26. Remove Duplicates from Sorted Array
+Easy
+11.1K
+15K
+Companies
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+*
+* */
